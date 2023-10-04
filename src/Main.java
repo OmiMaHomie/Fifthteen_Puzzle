edb81f8b0ffa1f2ae5ef588d9d84027b151a8884
Om Khadka <52017889+OmiMaHomie@users.noreply.github.com>
2023-10-03 21:13:49 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Will be the class where all the code executions will occur.
+ */
+public class Main
+{
+    /**
+     * Main method.
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+    }
+}
